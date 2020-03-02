d15eda593af5243e5e93fd48fa8823895e8c2657
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-03-02 02:00:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,10 @@ class Song
   
   @@all = []
   
-  def initialise(name, artist, genre)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
   end
     
   
